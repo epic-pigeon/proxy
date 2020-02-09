92d5074222a8392d2d8d4459d89b794b7d8f0a2b
nikita202 <nikita@ostaplyuk.com>
2020-02-09 15:28:26 +0200

    kar

--- proxy_index.js
+++ proxy_index.js
@@ -2,6 +2,19 @@ const http = require("http");
 const net = require("net");
 const process = require("process");
 
+
+function generateHttpResponse(response) {
+    response = response.toString();
+    return `HTTP/1.1 200 OK\n` +
+        `Date: ${new Date().toUTCString()}\n` +
+        `Server: Apache/2.2.14 (Win32)\n` +
+        `Last-Modified: ${new Date().toUTCString()}\n` +
+        `Content-Length: ${response.length}\n` +
+        `Content-Type: text/plain\n` +
+        `Connection: Closed\n\n` +
+        response;
+}
+
 net.createServer(function (socket) {
     let request = "";
     socket.on("data", data => {
@@ -18,15 +31,11 @@ net.createServer(function (socket) {
         let [requestType, url, protocol] = header.split(" ");
         if (requestType === "CONNECT") {
             console.log(`Connecting to ${url} via ${protocol}...`);
-            socket.write(
-                `HTTP/1.1 200 OK\nConnecting to ${url} via ${protocol}...`
-            );
+            socket.write(generateHttpResponse(`Connecting to ${url} via ${protocol}...`));
             socket.end();
         } else {
             console.log(`Wrong request type ${requestType}`);
-            socket.write(
-                `HTTP/1.1 200 OK\ngtfo`
-            );
+            socket.write(generateHttpResponse("gtfo"));
             socket.end();
         }
     }
