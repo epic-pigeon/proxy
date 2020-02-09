92cc2b1f1d0ad5992a5fb7d75694c78fba497e52
nikita202 <nikita@ostaplyuk.com>
2020-02-09 14:45:31 +0200

    kar

--- proxy_index.js
+++ proxy_index.js
@@ -3,5 +3,18 @@ const net = require("net");
 const process = require("process");
 
 net.createServer(function (socket) {
-    socket.pipe(process.stdout);
+    let request = "";
+    socket.on("data", data => request += data);
+    socket.on("end", () => {
+        let header = request.split("\n")[0];
+        let [requestType, url, protocol] = header.split(" ");
+        if (requestType === "CONNECT") {
+            console.log(`Connecting to ${url} via ${protocol}...`);
+            socket.write(`Connecting to ${url} via ${protocol}...`);
+            socket.end();
+        } else {
+            socket.write("gtfo");
+            socket.end();
+        }
+    });
 }).listen(8080);
