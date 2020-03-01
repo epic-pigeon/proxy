6bb5579b7799453535bf316724cba96030e3d7ee
nikita202 <nikita@ostaplyuk.com>
2020-03-01 14:17:01 +0200

    hz

--- proxy_index.js
+++ proxy_index.js
@@ -83,7 +83,6 @@ function generateHttpResponse(response, status = "200 OK") {
     return `HTTP/1.1 ${status}\r\n` +
         `Content-Length: ${response.length}\r\n` +
         `Content-Type: text/html\r\n` +
-        `Connection: close\r\n` +
         response;
 }
 
